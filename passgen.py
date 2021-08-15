47b3073adff272ccefa4ae38104acffd667e0ac9
Kartikey Khanna <75823562+itsksquare@users.noreply.github.com>
2021-08-15 13:38:57 +0530

    updated passgen.py

--- passgen.py
+++ passgen.py
@@ -8,24 +8,15 @@ root.resizable(0, 0)
 root.title("PASSWORD GENERATOR")
 root.configure(bg="#FFFFFF")
 
-#heading
 heading = Label(root, text = 'PASSWORD GENERATOR', font ='calibri 15 bold').pack()
 
 pass_len = IntVar()
 pass_str = StringVar()
 
-
-###select password length
 Label(root, text = 'PASSWORD LENGTH', font = 'calibri 10 bold').place(x = 50, y= 50)
 length = Spinbox(root, from_= 8, to_ = 32, textvariable = pass_len, width = 15).place(x = 200, y= 50)
-# YOUR PASSWORD
 Label(root, text = 'YOUR PASSWORD', font = 'calibri 10 bold').place(x = 50, y= 80)
 Entry(root, textvariable = pass_str).place(x = 200, y= 80)
-
-
-#####define function
-
-
 
 def Generator():
     password = ''
@@ -41,16 +32,10 @@ def Generator():
                                           string.punctuation)
     pass_str.set(password)
 
-
 def Copy_password():
     pyperclip.copy(pass_str.get())
-
 
 Button(root, text = "GENERATE", command = Generator).place(x = 100, y= 120)
 Button(root, text = 'COPY', command = Copy_password).place(x = 200, y= 120)
 
-
-# loop to run program
 root.mainloop()
-
-
